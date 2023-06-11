166bd4f9ab7743ea62adffb847f81616f9ac76de
unknowntrojan <14975032+unknowntrojan@users.noreply.github.com>
2023-06-11 21:53:23 +0200

    increase signature size in bench

--- benches/pattern.rs
+++ benches/pattern.rs
@@ -15,17 +15,26 @@ fn bench_pattern_1gig(b: &mut Bencher) {
     let mut test_pattern: Vec<u8> = (0..size).map(|_| rng.gen_range(0..=255)).collect();
 
     // let pattern = size / 2;
-    let pattern = size - 5;
+    let pattern = size - 15;
 
     test_pattern[pattern] = 0xDE;
     test_pattern[pattern + 1] = 0xAD;
     test_pattern[pattern + 2] = 0xFF;
     test_pattern[pattern + 3] = 0xBE;
     test_pattern[pattern + 4] = 0xEF;
+    test_pattern[pattern + 5] = 0xDE;
+    test_pattern[pattern + 6] = 0xAD;
+    test_pattern[pattern + 7] = 0xFF;
+    test_pattern[pattern + 8] = 0xBE;
+    test_pattern[pattern + 9] = 0xEF;
 
     b.iter(|| {
         assert_eq!(
-            PatternSearcher::new(&test_pattern, pattern!(0xDE, 0xAD, _, 0xBE, 0xEF)).next(),
+            PatternSearcher::new(
+                &test_pattern,
+                pattern!(0xDE, 0xAD, _, 0xBE, 0xEF, 0xDE, 0xAD, _, 0xBE, 0xEF)
+            )
+            .next(),
             Some(pattern)
         );
     });
